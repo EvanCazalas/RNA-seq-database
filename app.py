0f9a2450837a7b1ff683406c2b460de6cd9c64e6
EvanCazalas <cczalas1@memphis.edu>
2026-05-29 21:38:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 #data = { "GSE": ["GSE111842","GSE51827"], "Patients": [33,10], "Cancer":["Breast","Breast"]}
 #df = pd.DataFrame(data)
+df = pd.read_csv("CTC_RNA_Seq-DATA_Search.csv", encoding="latin1")
 
 st.title("CTC RNA-seq Database")
 
 st.dataframe(df)
-df = pd.read_csv("CTC_RNA_Seq-DATA_Search.csv", encoding="latin1")
+
