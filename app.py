60952b5e788f18b5fed0831099fe2d8e52046ad1
EvanCazalas <cczalas1@memphis.edu>
2026-05-29 21:37:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 import streamlit as st
 import pandas as pd
 
-data = { "GSE": ["GSE111842","GSE51827"], "Patients": [33,10], "Cancer":["Breast","Breast"]}
-df = pd.DataFrame(data)
+#data = { "GSE": ["GSE111842","GSE51827"], "Patients": [33,10], "Cancer":["Breast","Breast"]}
+#df = pd.DataFrame(data)
 
 st.title("CTC RNA-seq Database")
 
